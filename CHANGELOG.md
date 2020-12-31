91cc3d984ec369deba63da3625bcf7aa391c2dc9
igorfs10 <igorfs10@gmail.com>
2020-12-31 19:47:26 -0300

    muda lib de terminal

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,10 +4,11 @@ Lista de alterações do projeto.
 Esse formato é baseado no [Keep a Changelog](https://keepachangelog.com/pt/1.0.0/),
 e esse projeto adere à [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Não lançado]
+## [0.0.3] - 2020-12-31
 ### Modificado
 - Atualização da lib rayon
 - Aplicação de otimização opt-level3 para dependências
+- Usa a lib console no lugar da colour para corrigir falhas do console
 
 ## [0.0.2] - 2020-10-04
 ### Modificado
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,45 @@
 use std::env;
+use std::fs::read;
 use std::io;
 use std::io::prelude::*;
-use std::fs::{ read };
 use std::path::Path;
 
-use rayon::prelude::*;
-use colour::*;
+use console::style;
 use crc::{Crc, CRC_32_ISO_HDLC};
-
+use rayon::prelude::*;
 
 fn main() {
     // Carrega os argumentos enviados por linha de comando
     let args: Vec<String> = env::args().collect();
     let lista_arquivos = args[1..args.len()].to_vec();
-    
-    green_ln!("Lendo arquivos..\n");
 
-    lista_arquivos.par_iter()
-            .for_each(|caminho_arquivo| {
-                let nome_arquivo = Path::new(&caminho_arquivo).file_name().unwrap().to_str().unwrap();
-                let arquivo = read(&caminho_arquivo).expect("Não foi possível ler o arquivo.");
-                let crc = Crc::<u32>::new(&CRC_32_ISO_HDLC);
-                let checksum = crc.checksum(&arquivo);
+    println!("{}", style("Lendo arquivos..\n").green());
 
-                white_ln!("\nArquivo: {}\nCRC32:{:X}", nome_arquivo, checksum);
-            });
+    lista_arquivos.par_iter().for_each(|caminho_arquivo| {
+        let nome_arquivo = Path::new(&caminho_arquivo)
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap();
+        let arquivo = read(&caminho_arquivo).expect("Não foi possível ler o arquivo.");
+        let crc = Crc::<u32>::new(&CRC_32_ISO_HDLC);
+        let checksum = crc.checksum(&arquivo);
+
+        println!(
+            "{}",
+            style(format!("\nArquivo: {}\nCRC32:{:X}", nome_arquivo, checksum)).white()
+        );
+    });
     pause();
 }
 
 fn pause() {
     let mut stdin = io::stdin();
 
-    blue_ln!("\n\nFim.");
+    println!("{}", style("\n\nFim.").blue());
 
     // Queremos que o cursor fique no final da linha, então imprimimos sem uma linha nova
-    blue_ln!("Aperte enter para encerrar...");
+    println!("{}", style("Aperte enter para encerrar...").blue());
 
     // Lê um único byte e descarta
     let _ = stdin.read(&mut [0u8]).unwrap();
